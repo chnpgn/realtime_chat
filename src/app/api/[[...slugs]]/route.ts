73acbe4bb6096766444d551539b62cdba2ccd5dd
chan pagne <chnpgn@gmail.com>
2025-12-19 09:41:32 +0300

    Finished setting up Elysia & Tanstack

---
+++ src/app/api/[[...slugs]]/route.ts
@@ -0,0 +1,7 @@
+import { Elysia, t } from 'elysia'
+
+const app = new Elysia({ prefix: '/api' }).get('/user', {user: {name: 'Elysia'}})
+
+export const GET = app.fetch 
+export const POST = app.fetch 
+export type App = typeof app
